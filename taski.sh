df7490e7d6250b383892541183f6bb6625e0efeb
root <root@pop-os.localdomain>
2023-07-21 11:44:32 +0200

    to check

--- taski.sh
+++ taski.sh
@@ -3,8 +3,18 @@
 
 function LocateFolder(){
   echoMessage "Copy and paste in the terminal the location below"
-  whereis newtask.py
-  read = newLocation
+  
+  read newLocation
+
+  {
+    cd "$newLocation" 2>/dev/null
+    
+  }||{
+    echoError "$newLocation is not a valid location"
+    exit
+  }
+ 
+  echo "Now the problem is solved but be sure to locate the python file in the program's folder"
 }
 
 
@@ -42,17 +52,17 @@ then
   }||{
 
     
-    echoError "The file 'newtask.py' wich is used for creating new task can't be located" 
-    echoMessage "Would you like to run the troubleshoot (y/n)"
+    echoError "The file 'newtask.py' wich is used for creating new task can't be located. Would you like to run the troubleshoot? (y/n)"
 
 
-    read=userInput
-    if [ $userInput = "y" ];
+    read userInput
+    if [ "$userInput" = "y" ];
     then
       LocateFolder
 
     else
       echo "I'm sorry maybe you could help me get better on Github"
+      exit
     fi
 
   }
@@ -63,20 +73,25 @@ then
     python3 newtask.py 2>/dev/null
   }||{
     echoMessage "Looks like that Python is not installed in your machine, would you like to install it? (y/n)"
-    read=userInput
-    
+    read userInput
 
-    if [ $userInput = "y" ];
+    if [ "$userInput" = "y" ];
     then
 
       sudo apt update
       sudo apt install python3
 
       echoMessage "Python installed!"
+
+
+    elif [ "$userInput" = "n" ];
+    then
+      echo "I'm sorry maybe you could help me get better on Github"
+      exit
     fi
 
-  }
+   }
 
 else
-  echo "$command is not recognized as a valid input"
+   echo "Command not recognized"
 fi
